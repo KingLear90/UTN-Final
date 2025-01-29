534f3fc1defa7f3b346df5deba3f77c92348e546
Alan S. <lic.sanchezalan@gmail.com>
2025-01-29 13:49:46 -0300

    new /Contact style

--- front/src/Pages/Contact/Contact.tsx
+++ front/src/Pages/Contact/Contact.tsx
@@ -19,37 +19,36 @@ function Contact() {
   
   return (
     <App>
-        <form className="formLayout" onSubmit={handleSubmit}>
-            <h4 className="formTitle">¡CONTACTANOS!</h4>
+        <form id="contact" className="contact-container"  onSubmit={handleSubmit}>
+            <h4 className="contactTitle">¡CONTACTANOS!</h4>
 
-            <span className='alert'>Los campos con señalados con * son obligatorios</span>
+            <div className='alert'>
+              <span >Todos los campos son obligatorios.</span>
+            </div>
+            
+            <label htmlFor="firstName">Nombre</label>
+            <input type="text" name="firstName" className="contact-input"/>
 
-            <label htmlFor="firstName">Nombre* </label>
-            <input type="text" name="firstName"/>
+            <label htmlFor="email">Correo electrónico</label>
+            <input type="email" name="email" className="contact-input" />
 
-            <label htmlFor="lastName">Apellido</label>
-            <input type="text" name="lastName"  />
-
-            <label htmlFor="email">Correo electrónico *</label>
-            <input type="email" name="email" />
-
-            <label htmlFor="contact" className='contact'>¿Cómo preferís ser contactado/a? *</label>  
+            <label htmlFor="contact" className='select'>¿Cómo preferís ser contactado/a?</label>  
             <select className="form-select" name="contact">
                       <option selected>Selecciona una opción</option>
                       <option value="1">Por mail.</option>
                       <option value="2">No quiero que me contacten, sólo quería hacer un comentario. </option>
             </select>   
 
-            <label htmlFor="message">Tu mensaje/comentario * </label>
-            <textarea name="message" id="message" rows={6} placeholder="Escriba su mensaje..."/> 
+            <label htmlFor="message">Tu mensaje/comentario</label>
+            <textarea name="message" className='contact-textarea' id="message" rows={8} maxLength={250} placeholder="Escriba su mensaje..."/> 
 
-            <div className="formButtonContainer">
-              <button type="submit" className="btn btn-primary">Enviar</button>
-              <button type="reset" className="btn btn-dark">Borrar</button>
-            </div>
-            {/* Como thanks es true, se muestra el mensaje de agradecimiento */}
-            {thanks && <h4 className='thanksMsg'>¡Gracias por contactarnos!</h4>} 
+            <div className="contact-buttons">
+              <button type="submit" className="btn btn-primary py-2">Enviar</button>
+              <button type="reset" className="btn btn-danger py-2">Borrar</button>
+            </div> 
         </form>
+         {/* Como thanks es true, se muestra el mensaje de agradecimiento */}
+         {thanks && <h4 className='thanksMsg'>¡Gracias por contactarnos!</h4>} 
     </App>
   )
 }
