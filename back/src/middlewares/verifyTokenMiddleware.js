29d8c3bdd636e0489e9760136cbad24b3f36fbc2
Alan S. <lic.sanchezalan@gmail.com>
2025-02-02 17:45:15 -0300

    console.log's for debug

--- back/src/middlewares/verifyTokenMiddleware.js
+++ back/src/middlewares/verifyTokenMiddleware.js
@@ -1,17 +1,19 @@
+import { SECRET } from "../config.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 export const verifyTokenMiddleware = (req, res, next) => {
-    try {
+        console.log('Full Authorization header:', req.headers.authorization);
+        console.log('All headers:', req.headers);
         // Verifica si el token está en el header de autorización de la petición.
-        const authHeader = req.headers.authorization?.split(" ")[1];
-        
-        if (!authHeader) {
+    try {    
+        const token = req.headers.authorization?.split(" ")[1] || req.session.token;
+
+        if (!token) {
             return res.status(401).json({ message: "Token not found. Authorization denied." });
         }
 
-        const token = authHeader;
-        
-        const decoded = verifyToken(token); // Decodifica el token.
+        const decoded = verifyToken(token, SECRET); // Decodifica el token.
+        console.log('Successfully decoded token:', decoded);
         req.user = decoded;
         next();     // Si sale bien, sigue el flujo de la app.
     } catch (error) {
